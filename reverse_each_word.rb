47374c541c7c5fc58ba0d26cf9003880e17cb70b
Chance Smith <chancesmith97@hotmail.com>
2019-02-06 19:31:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(x)
   split_string = x.split("")
   reversed = []
-  x.size.times { reversed << split_string.shift}
-  reversed.join
+  x.size.times { reversed << split_string.pop}
+  
 
 
 end
