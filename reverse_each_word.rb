1e16f708ec125cd9102d8df061eff9ed66128072
Chance Smith <chancesmith97@hotmail.com>
2019-02-06 20:13:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,9 +6,5 @@ def reverse_each_word(x)
         reversed << split_string[i]
         i-=1
       end
-    new_string = " "
-    reversed.each do |x|
-       (reversed.shift).join("")
-    end
-    reversed
+reversed
 end
